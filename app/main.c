c22084da8343d6024bfe3db950cdb3087329903a
gagarin221072022 <101321783+gagarin221072022@users.noreply.github.com>
2022-03-31 22:16:14 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include<math.h>
 #include "solution.h"
 float x1, x2;
 int fl;
 
 int main()
 {
-   solution(1, 5, -41, &fl, &x1, &x2);
+   return(solution(1, 5, -41, &fl, &x1, &x2));
 }
