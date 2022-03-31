698b9af0275e6a274feb9ae83391bef9bede84aa
gagarin221072022 <101321783+gagarin221072022@users.noreply.github.com>
2022-03-31 22:07:30 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<math.h>
-#include<solution.h>
-float x1, x1;
+#include <stdio.h>
+#include "solution.h"
+float x1, x2;
 int fl;
 
-int main(){
-  solution(1, 1, 1, &fl, &x1, &x2);
+int main()
+{
+   solution(1, 5, -41, &fl, &x1, &x2);
 }
